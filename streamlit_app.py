e465986671e2dafbe8a96d31f60ec8a5d559e3e3
walter zesk <walter@conformlab.com>
2024-11-15 16:30:12 +0000

    preparing to add leafmap

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,19 @@ chart = (
 )
 tab2.altair_chart(chart, use_container_width=True)
 
+
+
+# m = leafmap.Map(center=[40, -100], zoom=4)
+# cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+# regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+
+# m.add_geojson(regions, layer_name="US Regions")
+# m.add_points_from_xy(
+#     cities,
+#     x="longitude",
+#     y="latitude",
+#     color_column="region",
+#     icon_names=["gear", "map", "leaf", "globe"],
+#     spin=True,
+#     add_legend=True,
+# )
