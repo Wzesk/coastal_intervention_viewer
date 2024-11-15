ed7e7c82828d65e5c22c42dfd33ceea9313855a7
wzesk <walter@conformlab.com>
2024-11-15 09:58:46 -0500

    simplified table for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,48 +26,42 @@ def load_data():
 df = load_data()
 
 # Show a multiselect widget with the genres using `st.multiselect`.
-genres = col1.multiselect(
+types = col1.multiselect(
     "Type",
-    df.genre.unique(),
-    ["Airport Development", "Extension", "Maritime Development", "Environmental Protection", "New island"],
+    df.Type.unique(),
+    default=["Airport Development", "Extension"],
 )
 
 # Show a slider widget with the years using `st.slider`.
 years = st.slider("Date", 2010, 2024, (2010, 2024))
 
-# # Filter the dataframe based on the widget input and reshape it.
-# df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-# df_reshaped = df_filtered.pivot_table(
-#     index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-# )
-# df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
 # Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+df_filtered = df[(df["Type"].isin(types))] #& (df["Date"].between(years[0], years[1]))]
 df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+    index="Date", columns="Type", values="Area_sqm", aggfunc="sum", fill_value=0
 )
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+df_reshaped = df_reshaped.sort_values(by="Date", ascending=False)
 
 
 # Display the data as a table using `st.dataframe`.
 col1.dataframe(
     df_reshaped,
     use_container_width=True,
-    column_config={"year": col1.column_config.TextColumn("Year")},
+    column_config={"Date": st.column_config.TextColumn("Date")},
 )
 
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-col2.altair_chart(chart, use_container_width=True)
+# # Display the data as an Altair chart using `st.altair_chart`.
+# df_chart = pd.melt(
+#     df_reshaped.reset_index(), id_vars="Date", var_name="Type", value_name="Area_sqm"
+# )
+# chart = (
+#     alt.Chart(df_chart)
+#     .mark_line()
+#     .encode(
+#         x=alt.X("Date:N", title="Year"),
+#         y=alt.Y("Area_sqm:Q", title="Gross earnings ($)"),
+#         color="Type:N",
+#     )
+#     .properties(height=320)
+# )
+# col2.altair_chart(chart, use_container_width=True)
