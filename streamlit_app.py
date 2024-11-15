cb88128e1ecb6da213ffc0fffde48b817a05a5af
walter zesk <walter@conformlab.com>
2024-11-15 17:09:22 +0000

    added map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+import leafmap.foliumap as leafmap
+
 
 # Show the page title and description.
 st.set_page_config(page_title="Our Anthropogenic Shorelines")
@@ -65,18 +67,19 @@ chart = (
 tab2.altair_chart(chart, use_container_width=True)
 
 
+with tab3:
+    m = leafmap.Map(center=[40, -100], zoom=4)
+    cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
-# m = leafmap.Map(center=[40, -100], zoom=4)
-# cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-# regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
-
-# m.add_geojson(regions, layer_name="US Regions")
-# m.add_points_from_xy(
-#     cities,
-#     x="longitude",
-#     y="latitude",
-#     color_column="region",
-#     icon_names=["gear", "map", "leaf", "globe"],
-#     spin=True,
-#     add_legend=True,
-# )
+    m.add_geojson(regions, layer_name="US Regions")
+    m.add_points_from_xy(
+        cities,
+        x="longitude",
+        y="latitude",
+        color_column="region",
+        icon_names=["gear", "map", "leaf", "globe"],
+        spin=True,
+        add_legend=True,
+    )
+    m.to_streamlit(height=500)
